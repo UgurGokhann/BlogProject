d5456dc40f4ed5b3f3630cc41cc1c947c79c99ca
Uğur GÖKHAN <ugurgokhan@outlook.com>
2023-03-16 04:35:53 +0300

    Add project files.

---
+++ BlogÖdev/Models/Data/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using BlogÖdev.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using BlogÖdev.ViewModels.Article;
+using BlogÖdev.SeedData;
+
+namespace BlogÖdev.Models.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserSeedData());
+            modelBuilder.ApplyConfiguration(new ArticleSeedData());
+            modelBuilder.Entity<Article>().Property(x => x.CreatedTime).HasDefaultValueSql("getutcdate()");
+        }
+
+        public DbSet<BlogÖdev.ViewModels.Article.ArticleViewModel> ArticleViewModel { get; set; }
+
+        public DbSet<BlogÖdev.ViewModels.Article.UpdateViewModel> UpdateViewModel { get; set; }
+    }
+}
